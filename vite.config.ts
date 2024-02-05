96e48d807f63ea0df7079805e88590a89b977167
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-02-05 07:54:55 +0100

    Add Vite PWA plugin to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,15 @@ import react from '@vitejs/plugin-react';
 import vercel from 'vite-plugin-vercel';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import million from 'million/compiler';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react(), vercel(), TanStackRouterVite()],
+  plugins: [
+    react(),
+    vercel(),
+    TanStackRouterVite(),
+    million.vite({ auto: true }),
+    VitePWA({ registerType: 'autoUpdate' }),
+  ],
 });
