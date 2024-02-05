e4786a6c7176b99f4ed89e1e188c5546b921a971
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-02-05 08:40:11 +0100

    Remove VitePWA plugin configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,54 +7,5 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    vercel(),
-    TanStackRouterVite(),
-    million.vite({ auto: true }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
-      },
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png', 'maskable_icon.png'],
-      manifest: {
-        name: 'anki-app',
-        short_name: 'anki-app',
-        description: 'for learning norsk',
-        icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'favicon',
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'favicon',
-          },
-          {
-            src: '/apple-touch-icon.png',
-            sizes: '180x180',
-            type: 'image/png',
-            purpose: 'apple touch icon',
-          },
-          {
-            src: '/maskable_icon.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-        theme_color: '#171717',
-        background_color: '#f0e7db',
-        display: 'standalone',
-        scope: '/',
-        start_url: '/',
-        orientation: 'portrait',
-      },
-    }),
-  ],
+  plugins: [react(), vercel(), TanStackRouterVite(), million.vite({ auto: true })],
 });
