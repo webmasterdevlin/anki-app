708c40cb2dffbe800cdc120833b57ee9fe5fa0e0
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-02-08 23:33:08 +0100

    Update navigation logo link and visibility

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -7,13 +7,13 @@ const Navigation = () => {
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center">
-            <div className="flex-shrink-0 text-emerald-300" aria-label="inmeta logo">
+            <Link to="/" className="flex-shrink-0 text-emerald-300" aria-label="inmeta logo">
               inmeta
-            </div>
+            </Link>
             <div className="ml-10 flex items-baseline gap-2 space-x-4">
               <Link
                 to="/"
-                className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                className="hidden rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white md:block"
                 aria-current="page"
               >
                 Home
