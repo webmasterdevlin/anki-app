0c48481731fa6a2b16b7a55796e5a6200379e8dd
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-03-15 14:42:24 +0100

    Refactor navigation component styles

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -4,8 +4,8 @@ import { root } from '../utils/routePaths';
 
 const Navigation = () => {
   return (
-    <nav className=" bg-gray-800" aria-label="Main navigation">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <nav className="bg-gray-800" aria-label="Main navigation">
+      <div className="mx-auto max-w-7xl px-4">
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center">
             <Link to="/" className="flex-shrink-0 text-emerald-300" aria-label="inmeta logo">
@@ -17,7 +17,7 @@ const Navigation = () => {
                   <Link
                     key={to}
                     to={to}
-                    className="hidden rounded-md px-3 py-2 text-sm capitalize  text-gray-300 hover:bg-gray-700 hover:text-white md:block"
+                    className="rounded-md px-3 py-2 text-sm capitalize  text-gray-300 hover:bg-gray-700 hover:text-white md:block"
                     aria-current="page"
                     activeProps={{ className: 'font-bold' }}
                   >
