9388b7ca1ec7208d0ba46c3496274cf7cbc83e8e
Devlin Duldulao <webmasterdevlin@gmail.com>
2025-11-05 19:44:18 +0100

    updated

--- src/data/words3.ts
+++ src/data/words3.ts
@@ -356,4 +356,154 @@ export const words: Question[] = [
     english: 'different functional variations',
     spanish: 'diferentes variaciones funcionales',
   },
+  {
+    norwegian: 'hårkur',
+    english: 'hair treatment',
+    spanish: 'tratamiento capilar',
+  },
+  {
+    norwegian: 'kan du døpe hjembyen din',
+    english: 'can you name your hometown',
+    spanish: '¿puedes nombrar tu ciudad natal?',
+  },
+  {
+    norwegian: 'det smaker forferdelig og beiskt',
+    english: 'it tastes terrible and bitter',
+    spanish: 'sabe terrible y amargo',
+  },
+  {
+    norwegian: 'jeg tvinger henne',
+    english: 'i force her',
+    spanish: 'la obligo',
+  },
+  {
+    norwegian: 'jeg hadde klart',
+    english: 'i had managed',
+    spanish: 'había logrado',
+  },
+  {
+    norwegian: 'å peke ham ut',
+    english: 'to point him out',
+    spanish: 'señalarlo',
+  },
+  {
+    norwegian: 'å bli tatt ut',
+    english: 'to be taken out',
+    spanish: 'ser sacado',
+  },
+  {
+    norwegian: 'landslaget',
+    english: 'the national team',
+    spanish: 'el equipo nacional',
+  },
+  {
+    norwegian: 'vise trofeet er kjekt',
+    english: 'showing the trophy is nice',
+    spanish: 'mostrar el trofeo es agradable',
+  },
+  {
+    norwegian: 'gjeveste trofeet',
+    english: 'most prestigious trophy',
+    spanish: 'trofeo más prestigioso',
+  },
+  {
+    norwegian: 'køle på deg',
+    english: 'cool on you',
+    spanish: 'enfriar en ti',
+  },
+  {
+    norwegian: 'hvi du godtar',
+    english: 'why you accept',
+    spanish: 'por qué aceptas',
+  },
+  {
+    norwegian: 'en romantisk aften ut',
+    english: 'a romantic evening out',
+    spanish: 'una noche romántica fuera',
+  },
+  {
+    norwegian: 'være ganske pinlig',
+    english: 'be quite embarrassing',
+    spanish: 'ser bastante embarazoso',
+  },
+  {
+    norwegian: 'to jærbuer',
+    english: 'two people from Jæren',
+    spanish: 'dos personas de Jæren',
+  },
+  {
+    norwegian: 'frir til meg',
+    english: 'appeals to me',
+    spanish: 'me atrae',
+  },
+  {
+    norwegian: 'det kan fort bli',
+    english: 'it can quickly become',
+    spanish: 'puede convertirse rápidamente',
+  },
+  {
+    norwegian: 'fjeset ditt på plakaten',
+    english: 'your face on the poster',
+    spanish: 'tu cara en el cartel',
+  },
+  {
+    norwegian: 'sumo bryter',
+    english: 'sumo wrestler',
+    spanish: 'luchador de sumo',
+  },
+  {
+    norwegian: 'hogger du mye ved på hytta',
+    english: 'do you chop a lot of wood at the cabin',
+    spanish: '¿cortas mucha leña en la cabaña?',
+  },
+  {
+    norwegian: 'økse',
+    english: 'axe',
+    spanish: 'hacha',
+  },
+  {
+    norwegian: 'har du fått kjeft',
+    english: 'have you been scolded',
+    spanish: '¿te han regañado?',
+  },
+  {
+    norwegian: 'rev meg litt i armen ørene',
+    english: 'tore me a little in the arm ears',
+    spanish: 'me rasgó un poco en los oídos del brazo',
+  },
+  {
+    norwegian: 'en drittunge hele veien',
+    english: 'a brat all the way',
+    spanish: 'un mocoso todo el camino',
+  },
+  {
+    norwegian: 'inn med kua',
+    english: 'in with the cow',
+    spanish: 'dentro con la vaca',
+  },
+  {
+    norwegian: 'er verre enn å slippe inn en elefant',
+    english: 'is worse than letting in an elephant',
+    spanish: 'es peor que dejar entrar a un elefante',
+  },
+  {
+    norwegian: 'hoftene og lyskene',
+    english: 'the hips and groins',
+    spanish: 'las caderas y las ingles',
+  },
+  {
+    norwegian: 'dere sklir bortover plenen',
+    english: 'you slide across the lawn',
+    spanish: 'te deslizas por el césped',
+  },
+  {
+    norwegian: 'du har skåret mål',
+    english: 'you have scored goals',
+    spanish: 'has marcado goles',
+  },
+  {
+    norwegian: 'ha beskyttelsesdrakt',
+    english: 'have protective suit',
+    spanish: 'tener traje protector',
+  },
 ];
