b017700ca3e4c08eb770e9223a1ea0493905a758
Devlin Duldulao <webmasterdevlin@gmail.com>
2025-11-22 16:43:38 +0100

    updated

--- words4.ts
+++ words4.ts
@@ -4,4 +4,330 @@ type Question = {
   spanish: string;
 };
 
-export const words: Question[] = [];
+export const words: Question[] = [
+  {
+    norwegian: "strålebehandles for kreft",
+    english: "to undergo radiation therapy for cancer",
+    spanish: "someterse a radioterapia por cáncer",
+  },
+  {
+    norwegian: "noe grums her i vannet",
+    english: "some sediment here in the water",
+    spanish: "algo de sedimento aquí en el agua",
+  },
+  {
+    norwegian: "forferdelig vær i dag",
+    english: "terrible weather today",
+    spanish: "clima terrible hoy",
+  },
+  {
+    norwegian: "så vidt",
+    english: "barely",
+    spanish: "apenas",
+  },
+  {
+    norwegian: "betennelsessmerte",
+    english: "inflammatory pain",
+    spanish: "dolor inflamatorio",
+  },
+  {
+    norwegian: "i leddene",
+    english: "in the joints",
+    spanish: "en las articulaciones",
+  },
+  {
+    norwegian: "det er parykk",
+    english: "it's a wig",
+    spanish: "es una peluca",
+  },
+  {
+    norwegian: "en stund nå",
+    english: "a while now",
+    spanish: "un rato ahora",
+  },
+  {
+    norwegian: "til bekkenveggene",
+    english: "to the pelvic walls",
+    spanish: "a las paredes pélvicas",
+  },
+  {
+    norwegian: "til lymfeknuter i bikiniområdet",
+    english: "to lymph nodes in the bikini area",
+    spanish: "a los ganglios linfáticos en el área del bikini",
+  },
+  {
+    norwegian: "jeg er på stadium 3",
+    english: "I am at stage 3",
+    spanish: "estoy en etapa 3",
+  },
+  {
+    norwegian: "livmorhalskreft deles opp in fire stadier",
+    english: "cervical cancer is divided into four stages",
+    spanish: "el cáncer de cuello uterino se divide en cuatro etapas",
+  },
+  {
+    norwegian: "har svulsten vokst inn i andre organer",
+    english: "has the tumor grown into other organs",
+    spanish: "¿ha crecido el tumor en otros órganos?",
+  },
+  {
+    norwegian: "belage seg på at det kan ta tid",
+    english: "expect that it may take time",
+    spanish: "esperar que pueda tomar tiempo",
+  },
+  {
+    norwegian: "det rakk jeg ikke å si",
+    english: "I didn't get to say that",
+    spanish: "no llegué a decir eso",
+  },
+  {
+    norwegian: "et faktum",
+    english: "a fact",
+    spanish: "un hecho",
+  },
+  {
+    norwegian: "regransket",
+    english: "re-examined",
+    spanish: "reexaminado",
+  },
+  {
+    norwegian: "antakelig burde vært fulgt opp",
+    english: "probably should have been followed up",
+    spanish: "probablemente debería haber sido seguido",
+  },
+  {
+    norwegian: "skuffet and sint på samme tid",
+    english: "disappointed and angry at the same time",
+    spanish: "decepcionado y enojado al mismo tiempo",
+  },
+  {
+    norwegian: "ummiddelbart",
+    english: "immediately",
+    spanish: "inmediatamente",
+  },
+  {
+    norwegian: "midlertidig",
+    english: "temporarily",
+    spanish: "temporalmente",
+  },
+  {
+    norwegian: "i fare",
+    english: "at risk",
+    spanish: "en riesgo",
+  },
+  {
+    norwegian: "en enorm feilmargin",
+    english: "a huge margin of error",
+    spanish: "un enorme margen de error",
+  },
+  {
+    norwegian: "har tenkt mye på",
+    english: "have thought a lot about",
+    spanish: "he pensado mucho en",
+  },
+  {
+    norwegian: "en hel befolkning",
+    english: "an entire population",
+    spanish: "una población entera",
+  },
+  {
+    norwegian: "rammet av livmorhalskreft",
+    english: "affected by cervical cancer",
+    spanish: "afectado por cáncer de cuello uterino",
+  },
+  {
+    norwegian: "har båret",
+    english: "have borne",
+    spanish: "he soportado",
+  },
+  {
+    norwegian: "uhelbredelig",
+    english: "incurable",
+    spanish: "incurable",
+  },
+  {
+    norwegian: "urovekkende oppdagelse",
+    english: "disturbing discovery",
+    spanish: "descubrimiento inquietante",
+  },
+  {
+    norwegian: "et skremmende ord",
+    english: "a frightening word",
+    spanish: "una palabra aterradora",
+  },
+  {
+    norwegian: "sannsynlig",
+    english: "likely",
+    spanish: "probable",
+  },
+  {
+    norwegian: "en stor svulst",
+    english: "a large tumor",
+    spanish: "un tumor grande",
+  },
+  {
+    norwegian: "er overlykkelig over at jeg",
+    english: "am overjoyed that I",
+    spanish: "estoy muy feliz de que yo",
+  },
+  {
+    norwegian: "det innført",
+    english: "it introduced",
+    spanish: "se introdujo",
+  },
+  {
+    norwegian: 'skal kreftsvulsten skannes med "PET/CT"',
+    english: 'the cancer tumor will be scanned with "PET/CT"',
+    spanish: 'el tumor canceroso será escaneado con "PET/CT"',
+  },
+  {
+    norwegian: "er fristed",
+    english: "is a sanctuary",
+    spanish: "es un santuario",
+  },
+  {
+    norwegian: "skaut",
+    english: "veil",
+    spanish: "velo",
+  },
+  {
+    norwegian: "han foran seg",
+    english: "him in front of him",
+    spanish: "él frente a él",
+  },
+  {
+    norwegian: "har krympet",
+    english: "has shrunk",
+    spanish: "ha encogido",
+  },
+  {
+    norwegian: "livmor",
+    english: "uterus",
+    spanish: "útero",
+  },
+  {
+    norwegian: "livmorhals",
+    english: "cervix",
+    spanish: "cérvix",
+  },
+  {
+    norwegian: "har oppklaring rundt",
+    english: "have clarification around",
+    spanish: "tener aclaración sobre",
+  },
+  {
+    norwegian: "å tolke",
+    english: "to interpret",
+    spanish: "interpretar",
+  },
+  {
+    norwegian: "hva sier pilene",
+    english: "what do the arrows say",
+    spanish: "¿qué dicen las flechas?",
+  },
+  {
+    norwegian: "forebygge 50 tilfeller av kreft",
+    english: "prevent 50 cases of cancer",
+    spanish: "prevenir 50 casos de cáncer",
+  },
+  {
+    norwegian: "er mange skjebner",
+    english: "are many destinies",
+    spanish: "son muchos destinos",
+  },
+  {
+    norwegian: "uten velsignelse fra",
+    english: "without the blessing of",
+    spanish: "sin la bendición de",
+  },
+  {
+    norwegian: "en fordel",
+    english: "an advantage",
+    spanish: "una ventaja",
+  },
+  {
+    norwegian: "er det prestisje",
+    english: "there is prestige",
+    spanish: "hay prestigio",
+  },
+  {
+    norwegian: "utgangspunktet her har vært",
+    english: "the starting point here has been",
+    spanish: "el punto de partida aquí ha sido",
+  },
+  {
+    norwegian: "er ingenting gærent med",
+    english: "there is nothing wrong with",
+    spanish: "no hay nada malo con",
+  },
+  {
+    norwegian: "å utvide testingen til 12 typer",
+    english: "to expand testing to 12 types",
+    spanish: "ampliar las pruebas a 12 tipos",
+  },
+  {
+    norwegian: "jeg blir så utålmodig",
+    english: "I get so impatient",
+    spanish: "me pongo tan impaciente",
+  },
+  {
+    norwegian: "er løst innen",
+    english: "is solved within",
+    spanish: "se resuelve dentro de",
+  },
+  {
+    norwegian: "innføre en",
+    english: "introduce a",
+    spanish: "introducir un",
+  },
+  {
+    norwegian: "primærsjekk",
+    english: "primary check",
+    spanish: "chequeo primario",
+  },
+  {
+    norwegian: "en omlegging",
+    english: "a change",
+    spanish: "un cambio",
+  },
+  {
+    norwegian: "stråling",
+    english: "radiation",
+    spanish: "radiación",
+  },
+  {
+    norwegian: "en fartsdump",
+    english: "a speed bump",
+    spanish: "un tope de velocidad",
+  },
+  {
+    norwegian: "hjertebank",
+    english: "palpitations",
+    spanish: "palpitaciones",
+  },
+  {
+    norwegian: "utfall",
+    english: "outcome",
+    spanish: "resultado",
+  },
+  {
+    norwegian: "et veikfryss",
+    english: "a crossroads",
+    spanish: "una encrucijada",
+  },
+  {
+    norwegian: "helt forferdelig å oppleve",
+    english: "absolutely terrible to experience",
+    spanish: "absolutamente terrible de experimentar",
+  },
+  {
+    norwegian: "for en lettelse",
+    english: "what a relief",
+    spanish: "qué alivio",
+  },
+  {
+    norwegian: "som erstatter mikroskopjekk",
+    english: "that replaces microscope check",
+    spanish: "que reemplaza la revisión del microscopio",
+  },
+];
