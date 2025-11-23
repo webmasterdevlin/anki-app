37394c9d80d9bbf19a4c846426d6c570840c154b
Devlin Duldulao <webmasterdevlin@gmail.com>
2025-11-23 13:06:06 +0100

    updated

--- words4.ts
+++ words4.ts
@@ -330,4 +330,429 @@ export const words: Question[] = [
     english: "that replaces microscope check",
     spanish: "que reemplaza la revisión del microscopio",
   },
+  {
+    norwegian: "du er herfra",
+    english: "you are from here",
+    spanish: "eres de aquí",
+  },
+  {
+    norwegian: "jeg er iallfall",
+    english: "I am at least",
+    spanish: "yo soy al menos",
+  },
+  {
+    norwegian: "avbrøt og tolken",
+    english: "interrupted and the interpreter",
+    spanish: "interrumpió y el intérprete",
+  },
+  {
+    norwegian: "har ventet på",
+    english: "have been waiting for",
+    spanish: "he estado esperando por",
+  },
+  {
+    norwegian: "helt fremmed kvinne",
+    english: "completely stranger woman",
+    spanish: "mujer completamente extraña",
+  },
+  {
+    norwegian: "de forfalsket papirene",
+    english: "they forged the papers",
+    spanish: "falsificaron los papeles",
+  },
+  {
+    norwegian: "utgitt seg i alle offentlige papirer",
+    english: "posed as in all official papers",
+    spanish: "se hizo pasar como en todos los documentos oficiales",
+  },
+  {
+    norwegian: "for å utgi seg",
+    english: "to impersonate",
+    spanish: "para hacerse pasar por",
+  },
+  {
+    norwegian: "utenlandske barn",
+    english: "foreign children",
+    spanish: "niños extranjeros",
+  },
+  {
+    norwegian: "at barn ikke utsettes",
+    english: "that children are not exposed",
+    spanish: "que los niños no estén expuestos",
+  },
+  {
+    norwegian: "er restende adopsjonsskandaler",
+    english: "are remaining adoption scandals",
+    spanish: "son escándalos de adopción restantes",
+  },
+  {
+    norwegian: "blitt avdekket i Europa",
+    english: "been uncovered in Europe",
+    spanish: "se ha descubierto en Europa",
+  },
+  {
+    norwegian: "bidratt til",
+    english: "contributed to",
+    spanish: "contribuido a",
+  },
+  {
+    norwegian: "innover meg",
+    english: "into me",
+    spanish: "dentro de mí",
+  },
+  {
+    norwegian: "utga seg for å være mor",
+    english: "pretended to be mother",
+    spanish: "fingió ser madre",
+  },
+  {
+    norwegian: "kom gjennombruddet",
+    english: "made the breakthrough",
+    spanish: "logró el avance",
+  },
+  {
+    norwegian: "et fødselsarmbånd fra sykehuset",
+    english: "a birth bracelet from the hospital",
+    spanish: "una pulsera de nacimiento del hospital",
+  },
+  {
+    norwegian: "er litt slåended",
+    english: "is a bit striking",
+    spanish: "es un poco sorprendente",
+  },
+  {
+    norwegian: "at hjertet dunket",
+    english: "that the heart was beating",
+    spanish: "que el corazón latía",
+  },
+  {
+    norwegian: "hun ble plasset på",
+    english: "she was placed on",
+    spanish: "ella fue colocada en",
+  },
+  {
+    norwegian: "du må forsørge",
+    english: "you must provide for",
+    spanish: "debes proveer para",
+  },
+  {
+    norwegian: "hun ble utsatt",
+    english: "she was subjected",
+    spanish: "ella fue sometida",
+  },
+  {
+    norwegian: "etterpå angret hun",
+    english: "afterwards she regretted",
+    spanish: "después se arrepintió",
+  },
+  {
+    norwegian: "ført ut av",
+    english: "taken out of",
+    spanish: "sacado de",
+  },
+  {
+    norwegian: "overleverte tre",
+    english: "handed over three",
+    spanish: "entregó tres",
+  },
+  {
+    norwegian: "etter avsløringen i",
+    english: "after the revelation in",
+    spanish: "después de la revelación en",
+  },
+  {
+    norwegian: "overbevise av",
+    english: "convince by",
+    spanish: "convencer por",
+  },
+  {
+    norwegian: "en granskning",
+    english: "an investigation",
+    spanish: "una investigación",
+  },
+  {
+    norwegian: "å midlertidig avslutte",
+    english: "to temporarily halt",
+    spanish: "detener temporalmente",
+  },
+  {
+    norwegian: "tvang",
+    english: "forced",
+    spanish: "forzado",
+  },
+  {
+    norwegian: "menneskehandel",
+    english: "human trafficking",
+    spanish: "trata de personas",
+  },
+  {
+    norwegian: "er entydig",
+    english: "is unequivocal",
+    spanish: "es inequívoco",
+  },
+  {
+    norwegian: "ble avdekket i",
+    english: "was uncovered in",
+    spanish: "se descubrió en",
+  },
+  {
+    norwegian: "å forfalske",
+    english: "to forge",
+    spanish: "falsificar",
+  },
+  {
+    norwegian: "som sådan",
+    english: "as such",
+    spanish: "como tal",
+  },
+  {
+    norwegian: "utnyttet folks fattigdom",
+    english: "exploited people's poverty",
+    spanish: "explotó la pobreza de la gente",
+  },
+  {
+    norwegian: "besynderlig",
+    english: "peculiar",
+    spanish: "peculiar",
+  },
+  {
+    norwegian: "å utelukke",
+    english: "to exclude",
+    spanish: "excluir",
+  },
+  {
+    norwegian: "enkelthendelser",
+    english: "individual incidents",
+    spanish: "incidentes individuales",
+  },
+  {
+    norwegian: "har forekommet en rekke ganger",
+    english: "has occurred several times",
+    spanish: "ha ocurrido varias veces",
+  },
+  {
+    norwegian: "troverdig",
+    english: "credible",
+    spanish: "creíble",
+  },
+  {
+    norwegian: "rastløsheten",
+    english: "the restlessness",
+    spanish: "la inquietud",
+  },
+  {
+    norwegian: "det lyktes aldri",
+    english: "it never succeeded",
+    spanish: "nunca tuvo éxito",
+  },
+  {
+    norwegian: "utstedte",
+    english: "issued",
+    spanish: "emitido",
+  },
+  {
+    norwegian: "det var  delvis tak over huset",
+    english: "there was partial roof over the house",
+    spanish: "había techo parcial sobre la casa",
+  },
+  {
+    norwegian: "å innbille seg",
+    english: "to imagine",
+    spanish: "imaginar",
+  },
+  {
+    norwegian: "uttrykket til de ansatte",
+    english: "the expression of the employees",
+    spanish: "la expresión de los empleados",
+  },
+  {
+    norwegian: "bestikkelser",
+    english: "bribes",
+    spanish: "sobornos",
+  },
+  {
+    norwegian: "belastningen",
+    english: "the burden",
+    spanish: "la carga",
+  },
+  {
+    norwegian: "jeg ble utsatt",
+    english: "I was subjected",
+    spanish: "fui sometido",
+  },
+  {
+    norwegian: "kan aldri gjenopprettes",
+    english: "can never be restored",
+    spanish: "nunca se puede restaurar",
+  },
+  {
+    norwegian: "mødre er feilaktig blitt fortalt at",
+    english: "mothers have been mistakenly told that",
+    spanish: "a las madres se les ha dicho erróneamente que",
+  },
+  {
+    norwegian: "på lovlig vis",
+    english: "in a legal way",
+    spanish: "de manera legal",
+  },
+  {
+    norwegian: "det medfører riktighet",
+    english: "it entails correctness",
+    spanish: "implica corrección",
+  },
+  {
+    norwegian: "adoptivforeldrene",
+    english: "the adoptive parents",
+    spanish: "los padres adoptivos",
+  },
+  {
+    norwegian: "det skyldes adopsjonssystemet",
+    english: "it is due to the adoption system",
+    spanish: "se debe al sistema de adopción",
+  },
+  {
+    norwegian: "det er forferdelig",
+    english: "it is terrible",
+    spanish: "es terrible",
+  },
+  {
+    norwegian: "å granske",
+    english: "to investigate",
+    spanish: "investigar",
+  },
+  {
+    norwegian: "han henviste til et nytt",
+    english: "he referred to a new",
+    spanish: "se refirió a un nuevo",
+  },
+  {
+    norwegian: "holdepunkter for å iverksette en gransking",
+    english: "grounds to initiate an investigation",
+    spanish: "motivos para iniciar una investigación",
+  },
+  {
+    norwegian: "et forsvarlig system",
+    english: "a responsible system",
+    spanish: "un sistema responsable",
+  },
+  {
+    norwegian: "å trekke seg",
+    english: "to resign",
+    spanish: "renunciar",
+  },
+  {
+    norwegian: "det oppleves illojalt",
+    english: "it feels disloyal",
+    spanish: "se siente desleal",
+  },
+  {
+    norwegian: "underleverandører av",
+    english: "subcontractors of",
+    spanish: "subcontratistas de",
+  },
+  {
+    norwegian: "virksomhet",
+    english: "business",
+    spanish: "negocio",
+  },
+  {
+    norwegian: "jevnlig av myndighetene",
+    english: "regularly by the authorities",
+    spanish: "regularmente por las autoridades",
+  },
+  {
+    norwegian: "byråene har visst",
+    english: "the agencies have known",
+    spanish: "las agencias han sabido",
+  },
+  {
+    norwegian: "staten formidler",
+    english: "the state mediates",
+    spanish: "el estado media",
+  },
+  {
+    norwegian: "vi kan utelukke",
+    english: "we can exclude",
+    spanish: "podemos excluir",
+  },
+  {
+    norwegian: "jeg inngår en",
+    english: "I enter into a",
+    spanish: "entro en un",
+  },
+  {
+    norwegian: "sårbarheter i et land",
+    english: "vulnerabilities in a country",
+    spanish: "vulnerabilidades en un país",
+  },
+  {
+    norwegian: "har fått innsyn",
+    english: "have gained insight",
+    spanish: "han obtenido información",
+  },
+  {
+    norwegian: "kunne få bukt",
+    english: "could get rid of",
+    spanish: "podría deshacerse de",
+  },
+  {
+    norwegian: "foreslått å stanse",
+    english: "suggested to stop",
+    spanish: "sugerido detener",
+  },
+  {
+    norwegian: "beslutning om",
+    english: "decision on",
+    spanish: "decisión sobre",
+  },
+  {
+    norwegian: "å feie",
+    english: "to sweep",
+    spanish: "barrer",
+  },
+  {
+    norwegian: "jeg kan forsikre deg",
+    english: "I can assure you",
+    spanish: "puedo asegurarte",
+  },
+  {
+    norwegian: "det er grusomme skjebner",
+    english: "there are terrible fates",
+    spanish: "hay destinos terribles",
+  },
+  {
+    norwegian: "siste brikkene på plass",
+    english: "the last pieces in place",
+    spanish: "las últimas piezas en su lugar",
+  },
+  {
+    norwegian: "vanvittig følelse",
+    english: "insane feeling",
+    spanish: "sensación de locura",
+  },
+  {
+    norwegian: "anledning til",
+    english: "opportunity to",
+    spanish: "oportunidad de",
+  },
+  {
+    norwegian: "mørkeste krokene",
+    english: "the darkest corners",
+    spanish: "los rincones más oscuros",
+  },
+  {
+    norwegian: "dypeste dalane",
+    english: "the deepest valleys",
+    spanish: "los valles más profundos",
+  },
+  {
+    norwegian: "å feie ting under teppet",
+    english: "to sweep things under the rug",
+    spanish: "barrer las cosas bajo la alfombra",
+  },
+  {
+    norwegian: "ellers oser av velstand",
+    english: "otherwise exudes prosperity",
+    spanish: "de lo contrario exuda prosperidad",
+  },
 ];
