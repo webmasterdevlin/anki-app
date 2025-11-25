0491f3a26c348ba580e508368e2f6f428c4d11c1
Devlin Duldulao <webmasterdevlin@gmail.com>
2025-11-25 15:57:20 +0100

    updated

--- words5.ts
+++ words5.ts
@@ -4,4 +4,290 @@ type Question = {
   spanish: string;
 };
 
-export const words: Question[] = [];
+export const words: Question[] = [
+  {
+    norwegian: "er alltid fæltmed dyr",
+    english: "is always awful with animals",
+    spanish: "siempre es terrible con los animales",
+  },
+  {
+    norwegian: "jag skal sage bort hele furen",
+    english: "I will saw away the whole pine tree",
+    spanish: "voy a cortar todo el pino",
+  },
+  {
+    norwegian: "en titanprotese",
+    english: "a titanium prosthesis",
+    spanish: "una prótesis de titanio",
+  },
+  {
+    norwegian: "noen hunder tåler ikke å hoppe ned fra sofaen",
+    english: "some dogs can't tolerate jumping down from the sofa",
+    spanish: "algunos perros no toleran saltar desde el sofá",
+  },
+  {
+    norwegian: "at du ikke kan unggå defektene",
+    english: "that you can't avoid the defects",
+    spanish: "que no puedes evitar los defectos",
+  },
+  {
+    norwegian: "som oppstår",
+    english: "that arise",
+    spanish: "que surgen",
+  },
+  {
+    norwegian: "går etter utssendet",
+    english: "goes after the appearance",
+    spanish: "va tras la apariencia",
+  },
+  {
+    norwegian: "dyrlege",
+    english: "veterinarian",
+    spanish: "veterinario",
+  },
+  {
+    norwegian: "hodeskalle",
+    english: "skull",
+    spanish: "cráneo",
+  },
+  {
+    norwegian: "nesen er avlet bort",
+    english: "the nose has been bred away",
+    spanish: "la nariz ha sido eliminada mediante cría",
+  },
+  {
+    norwegian: "har vi tuklet så mye med hundene",
+    english: "we have tampered so much with the dogs",
+    spanish: "hemos manipulado tanto a los perros",
+  },
+  {
+    norwegian: "det perfekte utseendet",
+    english: "the perfect appearance",
+    spanish: "la apariencia perfecta",
+  },
+  {
+    norwegian: "er dømt til å liv med lidelser",
+    english: "is doomed to a life of suffering",
+    spanish: "está condenado a una vida de sufrimiento",
+  },
+  {
+    norwegian: "kjente jeg ingen i",
+    english: "I didn't know anyone in",
+    spanish: "no conocía a nadie en",
+  },
+  {
+    norwegian: "pesingen ble verre",
+    english: "the panting got worse",
+    spanish: "el jadeo empeoró",
+  },
+  {
+    norwegian: "jeg gruer meg veldig",
+    english: "I'm very nervous",
+    spanish: "estoy muy nervioso",
+  },
+  {
+    norwegian: "det er jo helt sykt at",
+    english: "it is completely crazy that",
+    spanish: "es completamente loco que",
+  },
+  {
+    norwegian: "her er oppstarten",
+    english: "here is the startup",
+    spanish: "aquí está el inicio",
+  },
+  {
+    norwegian: "for sunn hundeavl",
+    english: "for healthy dog breeding",
+    spanish: "para la cría saludable de perros",
+  },
+  {
+    norwegian: "som driver med utstilling",
+    english: "who are involved in exhibitions",
+    spanish: "que se dedican a las exposiciones",
+  },
+  {
+    norwegian: "å kontrolle dårlig hundeavl",
+    english: "to control bad dog breeding",
+    spanish: "controlar la mala cría de perros",
+  },
+  {
+    norwegian: "rasestandarden",
+    english: "the breed standard",
+    spanish: "el estándar de la raza",
+  },
+  {
+    norwegian: "kort snute",
+    english: "short snout",
+    spanish: "hocico corto",
+  },
+  {
+    norwegian: "mangfold",
+    english: "diversity",
+    spanish: "diversidad",
+  },
+  {
+    norwegian: "til å avle seg ut av",
+    english: "to breed out of",
+    spanish: "para criar fuera de",
+  },
+  {
+    norwegian: "dyrebeskyttelsen vil forby avl på",
+    english: "the animal protection will ban breeding of",
+    spanish: "la protección animal prohibirá la cría de",
+  },
+  {
+    norwegian: "det er uenighet om at",
+    english: "there is disagreement that",
+    spanish: "hay desacuerdo en que",
+  },
+  {
+    norwegian: "det blir for trangt for",
+    english: "it becomes too tight for",
+    spanish: "se vuelve demasiado estrecho para",
+  },
+  {
+    norwegian: "hjernebrokk",
+    english: "brain hernia",
+    spanish: "hernia cerebral",
+  },
+  {
+    norwegian: "de væskefylte hulrom",
+    english: "the fluid-filled cavities",
+    spanish: "las cavidades llenas de líquido",
+  },
+  {
+    norwegian: "at enkelte hunder må avlives",
+    english: "that certain dogs must be euthanized",
+    spanish: "que ciertos perros deben ser sacrificados",
+  },
+  {
+    norwegian: "hvor tett slektskapet er",
+    english: "how close the relationship is",
+    spanish: "qué tan cercana es la relación",
+  },
+  {
+    norwegian: "kryssing av søskenbarn",
+    english: "crossing of cousins",
+    spanish: "cruce de primos",
+  },
+  {
+    norwegian: "om helsøsken får barn sammen",
+    english: "if full siblings have children together",
+    spanish: "si hermanos completos tienen hijos juntos",
+  },
+  {
+    norwegian: "helsøsken",
+    english: "full siblings",
+    spanish: "hermanos completos",
+  },
+  {
+    norwegian: "tilstanden",
+    english: "the condition",
+    spanish: "la condición",
+  },
+  {
+    norwegian: "hakeslepp",
+    english: "jaw drop",
+    spanish: "mandíbula caída",
+  },
+  {
+    norwegian: "utrydningstruet enn pandaen",
+    english: "more endangered than the panda",
+    spanish: "más en peligro de extinción que el panda",
+  },
+  {
+    norwegian: "som beregner innavlsgrad på hunder",
+    english: "that calculates the inbreeding coefficient of dogs",
+    spanish: "que calcula el coeficiente de endogamia de los perros",
+  },
+  {
+    norwegian: "en innavlsgrad på",
+    english: "an inbreeding coefficient of",
+    spanish: "un coeficiente de endogamia de",
+  },
+  {
+    norwegian: "har aldri vurdert å stille ut",
+    english: "have never considered exhibiting",
+    spanish: "nunca he considerado exhibir",
+  },
+  {
+    norwegian: "et individ",
+    english: "an individual",
+    spanish: "un individuo",
+  },
+  {
+    norwegian: "så nært opp til standerden som mulig",
+    english: "as close to the standard as possible",
+    spanish: "lo más cerca posible del estándar",
+  },
+  {
+    norwegian: "vi streber etter det",
+    english: "we strive for it",
+    spanish: "nos esforzamos por ello",
+  },
+  {
+    norwegian: "et litt surt uttrykk",
+    english: "a slightly sour expression",
+    spanish: "una expresión un poco agria",
+  },
+  {
+    norwegian: "å avle på",
+    english: "to breed for",
+    spanish: "criar para",
+  },
+  {
+    norwegian: "blant annet rettssaken som pågår nå",
+    english: "among other things the lawsuit that is ongoing now",
+    spanish: "entre otras cosas la demanda que está en curso ahora",
+  },
+  {
+    norwegian: "er sørgelig",
+    english: "is sad",
+    spanish: "es triste",
+  },
+  {
+    norwegian: "av useriøse folk",
+    english: "by unserious people",
+    spanish: "por gente poco seria",
+  },
+  {
+    norwegian: "at valpekjøpere og oppdrettere",
+    english: "that puppy buyers and breeders",
+    spanish: "que los compradores de cachorros y los criadores",
+  },
+  {
+    norwegian: "avviser at det er helseutfordringer",
+    english: "deny that there are health challenges",
+    spanish: "niegan que haya desafíos de salud",
+  },
+  {
+    norwegian: "fraråder kjøp av flere raser",
+    english: "discourages the purchase of several breeds",
+    spanish: "desaconseja la compra de varias razas",
+  },
+  {
+    norwegian: "det er noe i gjære",
+    english: "there is something brewing",
+    spanish: "hay algo en marcha",
+  },
+  {
+    norwegian: "har mange mopser pusteproblemer",
+    english: "many pugs have breathing problems",
+    spanish: "muchos pugs tienen problemas respiratorios",
+  },
+  {
+    norwegian: "det var veldig trange",
+    english: "it was very tight",
+    spanish: "estaba muy apretado",
+  },
+  {
+    norwegian: "har du fått",
+    english: "have you received",
+    spanish: "has recibido",
+  },
+  {
+    norwegian: "noe prisoverslag",
+    english: "some price estimate",
+    spanish: "alguna estimación de precio",
+  },
+];
